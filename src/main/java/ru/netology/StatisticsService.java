ba6e3ea74078aaac7bb096574edc39119504a489
TanyTany <avdtatyana@gmail.com>
2020-12-21 22:17:24 +0300

    edited check

--- src/main/java/ru/netology/StatisticsService.java
+++ src/main/java/ru/netology/StatisticsService.java
@@ -9,6 +9,7 @@ public class StatisticsService {
      */
     public long findMax(long[] incomes) {
         long current_max_index = 0;
+        //{12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12}
         long current_max = incomes[0];
         for (long income : incomes)
             if (current_max < income)
